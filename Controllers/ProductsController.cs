d9b9730373ff86d895939d41017c96c93534f4c7
Vitor J. Nicacio <nicaciov@outlook.com>
2024-04-29 19:25:10 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using TesteAPI.Models;
+
+[ApiController]
+[Route("[controller]")]
+public class ProductsController : ControllerBase
+{
+    private static List<Product> _products = new List<Product>
+    {
+        new Product { Id = 1, Name = "Product 1", Price = 10.99m },
+        new Product { Id = 2, Name = "Product 2", Price = 20.99m },
+        new Product { Id = 3, Name = "Product 3", Price = 30.99m }
+    };
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Product>> Get()
+    {
+        return _products;
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<Product> Get(int id)
+    {
+        var product = _products.FirstOrDefault(p => p.Id == id);
+        if (product == null)
+        {
+            return NotFound();
+        }
+        return product;
+    }
+
+    [HttpPost]
+    public IActionResult Post(Product product)
+    {
+        _products.Add(product);
+        return CreatedAtAction(nameof(Get), new { id = product.Id }, product);
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult Put(int id, Product product)
+    {
+        var existingProduct = _products.FirstOrDefault(p => p.Id == id);
+        if (existingProduct == null)
+        {
+            return NotFound();
+        }
+        existingProduct.Name = product.Name;
+        existingProduct.Price = product.Price;
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        var product = _products.FirstOrDefault(p => p.Id == id);
+        if (product == null)
+        {
+            return NotFound();
+        }
+        _products.Remove(product);
+        return NoContent();
+    }
+}
